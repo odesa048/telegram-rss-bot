c4918f4dd8ec10b44323a68e36a38536a6d4943f
odesa048 <dariabudkoffskaya@gmail.com>
2026-02-23 15:05:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,19 @@
-import feedparser
 import requests
 import os
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHANNEL = -1002453264440
-RSS_URL = "https://www.048.ua/rss"
+CHANNEL = -1002453264440  # твой ID
 
-feed = feedparser.parse(RSS_URL)
-
-if not feed.entries:
-    exit()
-
-post = feed.entries[0]
-
-text = f"📰 {post.title}\n\n{post.link}"
+text = "✅ ТЕСТ: бот успешно публикует в канал"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-requests.post(
+
+response = requests.post(
     url,
     data={
         "chat_id": CHANNEL,
-        "text": text,
-        "disable_web_page_preview": False,
-    },
+        "text": text
+    }
 )
+
+print(response.text)
