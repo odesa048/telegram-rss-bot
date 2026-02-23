be2b909d4bdc25909fd8f0876fb9131a968f06ea
odesa048 <dariabudkoffskaya@gmail.com>
2026-02-23 13:44:54 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import feedparser
+import requests
+import os
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL = "@odesa_048_ua"
+RSS_URL = "ВСТАВЬ_ТУТ_RSS"
+
+feed = feedparser.parse(RSS_URL)
+
+if not feed.entries:
+    exit()
+
+post = feed.entries[0]
+
+text = f"📰 {post.title}\n\n{post.link}"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+requests.post(
+    url,
+    data={
+        "chat_id": CHANNEL,
+        "text": text,
+        "disable_web_page_preview": False,
+    },
+)
